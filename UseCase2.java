45a0e0d1b6522fdba7708ce77d03728107ae40e0
Rjchavan3499 <59140623+Rjchavan3499@users.noreply.github.com>
2021-12-09 23:09:28 +0530

    Added RandomNumber

---
+++ UseCase2.java
@@ -0,0 +1,14 @@
+package SnakeAndLadder;
+
+public class UseCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to snake and ladder game");
+        int dice=(int) Math.floor(Math.random()*10)%7;
+        int resut=(dice==0?1:dice);
+		System.out.println("dice number is:"+resut);
+
+	}
+
+}
