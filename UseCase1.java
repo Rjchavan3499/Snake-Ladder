fef0436bcd93854d8ede2bcb810b98dff2781709
Rjchavan3499 <59140623+Rjchavan3499@users.noreply.github.com>
2021-12-09 22:59:37 +0530

    ADDED "UseCase1.java"

---
+++ UseCase1.java
@@ -0,0 +1,12 @@
+package SnakeAndLadder;
+
+public class UseCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      System.out.println("Welcome to snake and ladder and game");
+
+      System.out.println("you are at position zero");
+	}
+
+}
